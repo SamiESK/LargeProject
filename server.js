1af5cd7401f78ab78099a45a8a077410a5d29c83
drew-gits <69527486+drew-gits@users.noreply.github.com>
2021-04-15 00:45:23 -0400

    Still debugging

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+const app = require("./backend/app");
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+
+    console.log("Using frontend/build")
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "frontend", "build", "index.html")
+        );
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}.`);
+}); // start Node + Express server on port
